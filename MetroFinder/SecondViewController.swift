841a9fc5551e29abe2c6c32b50b3d37f4701341f
sanidhya70 <ksanidhya007@gmail.com>
2018-12-06 19:57:35 -0500

    Implement TableViewController to show list of Metro Station.

---
+++ MetroFinder/SecondViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SecondViewController.swift
+//  MetroFinder
+//
+//  Created by sanidhya on 12/6/18.
+//  Copyright © 2018 sanidhya. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UITableViewController {
+
+    let cellID="CellID"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title="Metro Stations"
+        navigationController?.navigationBar.prefersLargeTitles=true
+
+       tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell=tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        
+        cell.textLabel?.text="HI THERE"
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
