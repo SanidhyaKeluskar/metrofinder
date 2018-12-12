ba6d31d57fb2c2f8c74b3b8dff1919d8ae4a236c
sanidhya70 <ksanidhya007@gmail.com>
2018-12-11 23:27:47 -0500

    Loaded Favourites Table with images

---
+++ MetroFinder/MyFavouriteTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MyFavouriteTableViewCell.swift
+//  MetroFinder
+//
+//  Created by sanidhya on 12/11/18.
+//  Copyright © 2018 sanidhya. All rights reserved.
+//
+
+import UIKit
+
+class MyFavouriteTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var myFavouriteLandmarkImage: UIImageView!
+    @IBOutlet weak var myFavouriteLandmarkname: UILabel!
+    
+}
---
+++ MetroFinder/View Controllers/MyFavouriteViewController.swift
@@ -0,0 +1,70 @@
+//
+//  MyFavouriteViewController.swift
+//  MetroFinder
+//
+//  Created by sanidhya on 12/11/18.
+//  Copyright © 2018 sanidhya. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import AlamofireImage
+
+class MyFavouriteViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+  var workoutstwo = PersistenceManager.sharedInstance.fetchWorkouts()
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        landmarkNames.removeAll()
+        landmarkImages.removeAll()
+        for value in workoutstwo{
+            landmarkNames.append(value.name)
+            landmarkImages.append(value.url)
+        }
+        
+        for value in landmarkNames {
+            print(value)
+        }
+        for value in landmarkImages {
+            print(value)
+        }
+        tableView.reloadData()
+    }
+    
+
+
+}
+
+extension MyFavouriteViewController : UITableViewDataSource, UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return  workoutstwo.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyFavouriteLandmark") as! MyFavouriteTableViewCell
+        
+        cell.myFavouriteLandmarkname.text = workoutstwo[indexPath.row].name
+        
+        Alamofire.request(workoutstwo[indexPath.row].url).responseImage(completionHandler: { response in
+            
+            if var image = response.result.value{
+                DispatchQueue.main.async {
+                    cell.myFavouriteLandmarkImage.image = image
+                }
+                
+            }
+            
+        } )
+        
+        return cell
+    }
+}
