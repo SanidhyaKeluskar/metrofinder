aa9498b68c545c7531d1b4237281425c8760e597
sanidhya70 <ksanidhya007@gmail.com>
2019-02-24 02:39:39 -0500

    feature which allows user to take picture of a restaurant & the app will show ratings.

---
+++ MetroFinder/Models/VisionApiResponse.swift
@@ -0,0 +1,9 @@
+//
+//  VisionApiResponse.swift
+//  MetroFinder
+//
+//  Created by sanidhya on 12/14/18.
+//  Copyright © 2018 sanidhya. All rights reserved.
+//
+
+import Foundation
---
+++ MetroFinder/View Controllers/ImageFindViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageFindViewController.swift
+//  MetroFinder
+//
+//  Created by sanidhya on 12/14/18.
+//  Copyright © 2018 sanidhya. All rights reserved.
+//
+
+import UIKit
+
+class ImageFindViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
